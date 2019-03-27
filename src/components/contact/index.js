7d828b2f26d05bc74b79df55161c2d8dc84ecc45
Robson <przepiorkarobin@gmail.com>
2019-03-27 16:35:59 +0100

    fix contact form netlify

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,7 +1,58 @@
 import React, { Component } from 'react'
 import * as El from './style'
 
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
+
 export default class index extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+  }
+
+  onChangeInput = e => {
+    const { name, value } = e.target
+    this.setState(prevState => ({ ...prevState, [name]: value }))
+
+    e.preventDefault();
+  }
+
+  handleSubmit = e => {
+
+    const {name, email, message} = this.state;
+    
+    const that = this
+
+    console.log('this is what I post: ', this.state)
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...{name, email, message} })
+    })
+    .then((res) => {
+        console.log('res', res)
+          that.setState({
+              name: '',
+              email: '',
+              message: '',
+    
+          })
+
+      })
+    .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+  
   render() {
     return (
       <El.MainWrapper>
@@ -18,19 +69,26 @@ export default class index extends Component {
         </El.Wrapper>
         <El.ContactWrapper>
           
-        <form name="contact" method="POST" data-netlify="true">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>   
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
+        <form 
+          name="contact" 
+          method="POST" 
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+            <input type="hidden" name="bot-field" />
+
+            <label htmlFor="name">Your Name:</label>   
+            <input type="text" name="name"  onChange={this.onChangeInput}/>
+
+            <label htmlFor="email"> Your Email: </label>
+            <input type="email" name="email" onChange={this.onChangeInput} />
+
+            <label htmlFor="message">Message: </label>
+            <textarea name="message" onChange={this.onChangeInput}></textarea>
+
             <button type="submit">Send</button>
-          </p>
+
         </form>
         </El.ContactWrapper>
 
